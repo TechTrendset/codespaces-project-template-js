4e16468daefe6242d528ca1cded6ff3f007d5399
TechTrendset <132773726+TechTrendset@users.noreply.github.com>
2024-06-13 01:36:13 +0500

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/redfloweer.jpg";
+import image from "../images/redflower.jpg";
 
 const imageAltText = "Pareesa Abbasi Software Developer & AI ethausist, Having enterprenuer spirit";
 
